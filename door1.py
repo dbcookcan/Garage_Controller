9d241084f38e93d3e4d0003c71191114ee757f71
“dbcookcan” <dbc_github@advan.ca>
2021-06-04 21:19:35 -0400

    Added door1/2 test programs to validate relay control.

---
+++ door1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import time
+
+import automationhat
+
+
+if automationhat.is_automation_hat():
+    automationhat.light.power.write(1)
+
+automationhat.relay.one.on()
+time.sleep(0.5)
+automationhat.relay.one.off()
+
+#
+# EOF
+#
+
--- test_read_am2302.py
+++ test_read_am2302.py
@@ -9,7 +9,8 @@ DHT_PIN=15
 
 h,t = dht.read(DHT_TYPE, DHT_PIN)
 print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
-
+print(h)
+print(t)
 
 #
 # EOF
